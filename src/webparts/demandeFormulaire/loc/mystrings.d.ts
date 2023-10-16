2e9a795dee79a3862f8e060383238cceeff7a9f0
Ghassen_khammessi <ghassenkhammessi123@icloud.com>
2023-10-16 10:12:44 +0100

    initialise the project

---
+++ src/webparts/demandeFormulaire/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface IDemandeFormulaireWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'DemandeFormulaireWebPartStrings' {
+  const strings: IDemandeFormulaireWebPartStrings;
+  export = strings;
+}
